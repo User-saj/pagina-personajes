407153a3926c045d5e71a7aae99ab9ad4a72fe90
User-saj <92058854+User-saj@users.noreply.github.com>
2023-10-30 19:49:16 -0600

    Add files via upload

--- actividad_04_extra.js
+++ actividad_04_extra.js
@@ -1,4 +1,4 @@
-/* JavaScript para Server Side actividad_04.html */
+/* JavaScript para Server Side actividad_04_extra.html */
 
 /* ------------------------ acciones iniciales --------------------------- */
 // variable para manejar el <span>> "numPsj"
@@ -13,8 +13,10 @@ var infoBoton = document.getElementById("obtnPsj");
 // bloquea el boton si no hay inf guardada
 if (numeroPsjs == 0) {
   infoBoton.disabled = true;
+  infoBoton.style.cursor = "not-allowed";
 } else {
   infoBoton.disabled = false;
+  infoBoton.style.cursor = "pointer";
 }
 // var para manejar al contenedor de ino del personaje
 var contInf = document.getElementById("infInd");
@@ -32,9 +34,9 @@ function func_guardaLS(evento) {
   let personaje, info_personaje;
   // carga la informacion del formulario en variables locales
   let nCompl = document.getElementById("nombre").value;
-  let tempMasc = document.getElementById("radioMasc"); // ".checked" regresa: true or false
+  let tempMasc = document.getElementById("radioMasc").checked; // .checked regresa: true/false
   // si el valor es: true/false se asigna a gnr: masculino/femenino
-  let gnr = tempMasc.checked ? "Masculino" : "Femenino"; // if de una linea
+  let gnr = tempMasc ? "Masculino" : "Femenino"; // if de una linea
   let pNac = document.getElementById("paisNac").value;
   let fNac = document.getElementById("fechNac").value;
   let espec = document.getElementById("especialidad").value;
@@ -57,10 +59,11 @@ function func_guardaLS(evento) {
   cont++;
   // despliega el numero de entradas guardadas en el Local Storage
   spanPsjs.innerHTML = localStorage.length;
-  // recarga la lista de personajes cada vez ques e agrega uno
+  // reconstruye la lista de personajes cada vez ques e agrega uno
   cargPsj();
   // habilta el boton info de personaje
   infoBoton.disabled = false;
+  infoBoton.style.cursor = "pointer";
   // oculta el contenedor de info del personaje
   contInf.style.display = "none";
 }
@@ -72,17 +75,15 @@ function cargPsj() {
   // var para manejar al selector de la pagina html
   let selector = document.getElementById("selecc");
   // borra todos los elementos previos de la lista de seleccion
-  while (selector.options.length > 0) {
-    selector.remove(0);
-  }
+  while (selector.options.length > 0) selector.remove(0); // while de una line no requiere {}
   // keys de todos los personajes guardados
-  var idsPersonajes = Object.keys(localStorage);
+  let idsPersonajes = Object.keys(localStorage);
   // recorre todos los personajes guardados y extrae su key y nombre
   for (const idPsj of idsPersonajes) {
     //  recupera la info de cada personaje en una var objeto
     let personaje_k = JSON.parse(localStorage.getItem(idPsj));
     // crea opcion (personaje por iteracion) para agregar al selector
-    var opcion = document.createElement("option");
+    let opcion = document.createElement("option");
     // asigna valor y nombre a la opcion
     opcion.value = idPsj;
     opcion.text = personaje_k.nombre;
